9d3518c0e5fcf2c638bc0d6481686051ea83500c
CJ Battey <cjbattey@gmail.com>
2020-05-08 23:18:27 -0700

    dropping extra columns from sample data

--- scripts/plotvae.py
+++ scripts/plotvae.py
@@ -6,13 +6,20 @@ import argparse
 parser=argparse.ArgumentParser()
 parser.add_argument("--latent_coords",help="path to latent_coords.txt file")
 parser.add_argument("--metadata",help="path to metadata file with column named 'sampleID'")
-parser.add_argument("--colorby",default=None,help="column name for a metadata field to color points. \
-                                      Must be character with <13 unique values.")
+parser.add_argument("--colorby",default=None,help="<optional> column name for a metadata field to color points. \
+                                      Must be character with < 16 unique values.")
+parser.add_argument("--outfile",default=None,help="<optional> output path.")
 args=parser.parse_args()
 
 latent_coords=args.latent_coords
 metadata=args.metadata
 colorby=args.colorby
+outfile=args.outfile
+
+# latent_coords="/Users/cj/popvae/out/ag1000g/ag1000g_1e5snps_latent_coords.txt"
+# metadata="/Users/cj/popvae/data/ag1000g/anopheles_samples_sp.txt"
+# colorby="country"
+# len(pd.factorize(meta[colorby])[1])
 
 #load data
 ld=pd.read_csv(latent_coords,sep="\t")
@@ -23,22 +30,19 @@ meta=pd.read_csv(metadata,sep="\t")
 ld=ld.merge(meta,on="sampleID")
 
 if not colorby==None:
-    if len(pd.factorize(ld[colorby])[1])>12:
-        print("color column must have <13 unique values")
+    if len(pd.factorize(ld[colorby])[1])>16:
+        print("color column must have <16 unique values")
         exit()
 
-#add hover panels
-tooltips=[(x,'@'+x) for x in ld.columns if not x in oldcols]
-hover = bokeh.models.HoverTool(tooltips=tooltips)
-
-#colors
+#plot
 p = bokeh.plotting.figure(plot_width=900, plot_height=600,
                           x_axis_label='LD1',
                           y_axis_label='LD2')
 
 if not colorby==None:
     pal=["#A6CEE3","#1F78B4","#B2DF8A","#33A02C","#FB9A99","#E31A1C",
-         "#FDBF6F","#FF7F00","#CAB2D6","#6A3D9A","#FFFF99","#B15928"]
+         "#FDBF6F","#FF7F00","#CAB2D6","#6A3D9A","#FFFF99","#B15928",
+         "#d442f5","#403f40","#291dad"]
     keys=np.array(pd.factorize(ld[colorby])[1])
     cmap={keys[x]:pal[x] for x in range(len(pd.factorize(ld[colorby])[1]))}
     for key, group in ld.groupby(colorby):
@@ -47,7 +51,13 @@ if not colorby==None:
                  color=cmap[key],legend=key)
 else:
     p.circle(x="LD1",y="LD2",size=7,alpha=0.75,source=ld,color="black")
+
+#hover panels
+tooltips=[(x,'@'+x) for x in ld.columns if not x in oldcols]
+hover = bokeh.models.HoverTool(tooltips=tooltips)
 p.add_tools(hover)
 
+if not outfile==None:
+    bokeh.io.output_file(filename=outfile,mode="inline")
 bokeh.io.show(p)
 bokeh.io.reset_output()
